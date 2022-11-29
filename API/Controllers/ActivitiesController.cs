2b3ef47a4b0778fa102206c0596277c8206a4ca4
jarekmadczak <miszorjest@gmail.com>
2022-11-29 19:16:21 +0100

    module2

---
+++ API/Controllers/ActivitiesController.cs
@@ -0,0 +1,26 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Presistence;
+using Domain;
+namespace API.Controllers
+{
+    public class ActivitiesController : BaseApiController
+    {
+        private readonly DataContext _context;
+        public ActivitiesController(DataContext context)
+        {
+            _context=context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Domain.Activity>>> GetActivities(){
+            return await _context.Activities.ToListAsync();
+        }
+        [HttpGet("{id}")] //activities/id 
+        public async Task<ActionResult<Domain.Activity>>GetActivity(Guid id){
+            return await _context.Activities.FindAsync(id);
+
+        }
+    }
+}
---
+++ API/Controllers/BaseApiController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")] //controler - słowo conmtroleer // localhost:5000/api/activities
+    public class BaseApiController : ControllerBase
+    {
+        
+    }
+}
